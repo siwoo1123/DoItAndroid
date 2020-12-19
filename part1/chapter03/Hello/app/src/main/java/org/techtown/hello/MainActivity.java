08a18b7e1a462b04649989eafea5a84c0c106196
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2020-12-19 17:48:28 +0900

    Update MainActivity.java

--- part1/chapter03/Hello/app/src/main/java/org/techtown/hello/MainActivity.java
+++ part1/chapter03/Hello/app/src/main/java/org/techtown/hello/MainActivity.java
@@ -1,11 +1,10 @@
-package org.techtown.hello;
+package com.example.hello;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,15 +18,12 @@ public class MainActivity extends AppCompatActivity {
     public void onButton1Clicked(View v) {
         Toast.makeText(this, "확인1 버튼이 눌렸어요.", Toast.LENGTH_LONG).show();
     }
-
     public void onButton2Clicked(View v) {
         Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
         startActivity(myIntent);
     }
-
     public void onButton3Clicked(View v) {
         Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
         startActivity(myIntent);
     }
-
 }
